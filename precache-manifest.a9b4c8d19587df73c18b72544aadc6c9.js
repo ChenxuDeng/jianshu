a6afbd665aacd3a7d70465317619b040d931bc3d
jimmy <836081346@qq.com>
2021-06-27 19:00:03 +0800

    Updates

---
+++ precache-manifest.a9b4c8d19587df73c18b72544aadc6c9.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c0009a05166d19eb3c605cfe7d2afe",
+    "url": "/jianshu/index.html"
+  },
+  {
+    "revision": "2f2a3985e3bb4c72b72b",
+    "url": "/jianshu/static/css/2.32895dee.chunk.css"
+  },
+  {
+    "revision": "8ecb6f131ea476e53269",
+    "url": "/jianshu/static/css/main.32ae480d.chunk.css"
+  },
+  {
+    "revision": "2f2a3985e3bb4c72b72b",
+    "url": "/jianshu/static/js/2.f4fe3ea8.chunk.js"
+  },
+  {
+    "revision": "f9bac094462f96fa27f57cc96b6a009c",
+    "url": "/jianshu/static/js/2.f4fe3ea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ecb6f131ea476e53269",
+    "url": "/jianshu/static/js/main.4cd10893.chunk.js"
+  },
+  {
+    "revision": "07f89e758489f2e84ab1",
+    "url": "/jianshu/static/js/runtime-main.36961724.js"
+  },
+  {
+    "revision": "b2e2caa2aec8ff89bd6957f09b4e6fce",
+    "url": "/jianshu/static/media/app content1.b2e2caa2.png"
+  },
+  {
+    "revision": "378196f0f0c87fee66210c957722605c",
+    "url": "/jianshu/static/media/app content2.378196f0.png"
+  },
+  {
+    "revision": "a3e7f05fee99976afbb936eb6d3c288a",
+    "url": "/jianshu/static/media/app content3.a3e7f05f.png"
+  },
+  {
+    "revision": "ed9583c5975a6d953f47c09ed63b911b",
+    "url": "/jianshu/static/media/app content4.ed9583c5.png"
+  },
+  {
+    "revision": "7dfef21cef3585139d67481fbeb3a488",
+    "url": "/jianshu/static/media/appphone.7dfef21c.png"
+  },
+  {
+    "revision": "e6e244a25f15a58bc91ceb4ea6d0e70a",
+    "url": "/jianshu/static/media/footer2.e6e244a2.png"
+  },
+  {
+    "revision": "57fe174be588966e9ae70967539666e3",
+    "url": "/jianshu/static/media/footer3.57fe174b.jpg"
+  },
+  {
+    "revision": "10518f0f6b33635180b190975ae68ca6",
+    "url": "/jianshu/static/media/footer4.10518f0f.jpg"
+  },
+  {
+    "revision": "ac7ba4ab204803bb4310f95d966d7804",
+    "url": "/jianshu/static/media/rec1.ac7ba4ab.webp"
+  },
+  {
+    "revision": "d81d293455b233cfe3c48e6e3e83d99b",
+    "url": "/jianshu/static/media/rec2.d81d2934.webp"
+  },
+  {
+    "revision": "2f377f1705ac41c8bd1d28e1c3eb4ffb",
+    "url": "/jianshu/static/media/rec3.2f377f17.webp"
+  },
+  {
+    "revision": "13cc0dace7f91fff7bec115695dda1a4",
+    "url": "/jianshu/static/media/rec4.13cc0dac.webp"
+  },
+  {
+    "revision": "dc7525c8d5290ad60a9b2a30889807af",
+    "url": "/jianshu/static/media/rec5.dc7525c8.webp"
+  },
+  {
+    "revision": "b31442523f899b9cfe392941b08d540b",
+    "url": "/jianshu/static/media/rec6.b3144252.png"
+  },
+  {
+    "revision": "03cc8798d5cc3f986e49cbcb2eb63079",
+    "url": "/jianshu/static/media/recommend.03cc8798.png"
+  },
+  {
+    "revision": "ac6f1056d92275f438eac0cbdd47b761",
+    "url": "/jianshu/static/media/wechat.ac6f1056.svg"
+  }
+]);
